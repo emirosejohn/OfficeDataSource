e4ecc790512afd63ed3379c0b1b672bb42fe4966
Max Booth <Max.Booth@dimensional.com>
2017-07-05 16:49:16 -0500

    debugged SQL

--- data/mssql/OfficeLocationMicroservice/Up/0001_CREATETABLES.sql
+++ data/mssql/OfficeLocationMicroservice/Up/0001_CREATETABLES.sql
@@ -8,71 +8,69 @@ CREATE TABLE [OfficeLocation].[Offices](
 	[Switchboard] [nvarchar](255),
 	[Fax] [nvarchar](255),
 	[TimeZone] [nvarchar](255),
-	[Operating] [boolean]
+	[Operating] [bit]
 )
 GO
-
-
 
 INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
 	[Operating]) VALUES ('Amsterdam', '***REMOVED*** ***REMOVED***',
-	'Netherlands','***REMOVED***', '***REMOVED***', 'CEST', True);
+	'Netherlands','***REMOVED***', '***REMOVED***', 'CEST', 1);
 GO
 
 INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
 	[Operating]) VALUES ('Austin','***REMOVED***
 	***REMOVED*** ***REMOVED***','United States','***REMOVED***',
-	'+***REMOVED***','CST', True);
+	'+***REMOVED***','CST', 1);
 
 
 INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
 	[Operating]) VALUES ('Berlin','***REMOVED*** Kurfürstendamm 194, 
-	***REMOVED***','Germany','***REMOVED***','***REMOVED***','CET', True);
+	***REMOVED***','Germany','***REMOVED***','***REMOVED***','CET', 1);
 
 INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
 	[Operating]) VALUES ('Charlotte','201 North Tryon Street, ste. 
-	2300 ***REMOVED***','United States','***REMOVED***','***REMOVED***','EST', True);
+	2300 ***REMOVED***','United States','***REMOVED***','***REMOVED***','EST', 1);
 
 INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
 	[Operating]) VALUES ('London','***REMOVED*** ***REMOVED***',
-	'United Kingdom','***REMOVED***','***REMOVED***','BST', True);
+	'United Kingdom','***REMOVED***','***REMOVED***','BST', 1);
 
 INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
 	[Operating]) VALUES ('Melbourne','***REMOVED*** ***REMOVED***',
-	'Australia','***REMOVED***','','AEST', True);
+	'Australia','***REMOVED***','','AEST', 1);
 
 INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
 	[Operating]) VALUES ('Santa Monica','***REMOVED*** ***REMOVED***',
-	'United States','***REMOVED***','***REMOVED***','PST', True);
+	'United States','***REMOVED***','***REMOVED***','PST', 1);
 
 INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
 	[Operating]) VALUES ('Singapore','***REMOVED*** ***REMOVED***',
-	'Singapore','***REMOVED***','***REMOVED***','SGT', True);
+	'Singapore','***REMOVED***','***REMOVED***','SGT', 1);
 
 INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
 	[Operating]) VALUES ('Sydney','***REMOVED*** ***REMOVED***',
-	'Australia','***REMOVED***','***REMOVED***','AEST', True);
+	'Australia','***REMOVED***','***REMOVED***','AEST', 1);
 
 INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
 	[Operating]) VALUES ('Tokyo','***REMOVED*** ***REMOVED*** ***REMOVED***',
-	'Japan','***REMOVED***','***REMOVED***','JST', True);
+	'Japan','***REMOVED***','***REMOVED***','JST', 1);
 
 INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
 	[Operating]) VALUES ('Toronto','***REMOVED*** ***REMOVED***',
-	'Canada','***REMOVED***','***REMOVED***','EDT', True);
+	'Canada','***REMOVED***','***REMOVED***','EDT', 1);
 
 INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
 	[Operating]) VALUES ('Vancouver','***REMOVED*** ***REMOVED******REMOVED***',
-	'Canada','***REMOVED***','***REMOVED***','PDT', True);
+	'Canada','***REMOVED***','***REMOVED***','PDT', 1);
