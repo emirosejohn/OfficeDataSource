377d9be62a2684bc56dc951a103ecb176d19c413
Emily Johnson <Emily.Johnson@dimensional.com>
2017-07-05 15:29:25 -0500

    added API controller

---
+++ src/WebUI/Controllers/OfficeLocationAPIController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OfficeLocationMicroservice.WebUi.Controllers
+{
+
+
+    /*
+     *  public OfficeLocationAPIController(IRegionSchemeRepository regionSchemeRepository,
+            IRegionSchemeByCodeTypeRepository regionSchemeByCodeTypeRepository )
+        {
+            _regionSchemeRepository = regionSchemeRepository;
+            _regionSchemeByCodeTypeRepository = regionSchemeByCodeTypeRepository;
+            _schemegateway = new SchemeGateway(new APISettings());
+        }
+        
+         */
+    public class OfficeLocationAPIController : ApiController
+    {
+    }
+}
