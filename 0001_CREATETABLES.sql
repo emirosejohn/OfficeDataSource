56da860d568d397b086dc44eb230f2ac0ed1c811
Emily Johnson <Emily.Johnson@dimensional.com>
2017-07-05 09:44:33 -0500

    populate sql table. not tested

--- 0001_CREATETABLES.sql
+++ 0001_CREATETABLES.sql
@@ -14,8 +14,65 @@ GO
 
 
 
-INSERT [OfficeLocation].[Offices] ([Name], [Address], 
+INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
 	[Country], [Switchboard], [Fax], [TimeZone], 
-	[Operating]) VALUES (N'Amsterdam', '***REMOVED*** ***REMOVED***',
+	[Operating]) VALUES ('Amsterdam', '***REMOVED*** ***REMOVED***',
 	'Netherlands','***REMOVED***', '***REMOVED***', 'CEST', True);
 GO
+
+INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
+	[Country], [Switchboard], [Fax], [TimeZone], 
+	[Operating]) VALUES ('Austin','***REMOVED***
+	***REMOVED*** ***REMOVED***','United States','***REMOVED***',
+	'+***REMOVED***','CST', True);
+
+
+INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
+	[Country], [Switchboard], [Fax], [TimeZone], 
+	[Operating]) VALUES ('Berlin','***REMOVED*** Kurfürstendamm 194, 
+	***REMOVED***','Germany','***REMOVED***','***REMOVED***','CET', True);
+
+INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
+	[Country], [Switchboard], [Fax], [TimeZone], 
+	[Operating]) VALUES ('Charlotte','201 North Tryon Street, ste. 
+	2300 ***REMOVED***','United States','***REMOVED***','***REMOVED***','EST', True);
+
+INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
+	[Country], [Switchboard], [Fax], [TimeZone], 
+	[Operating]) VALUES ('London','***REMOVED*** ***REMOVED***',
+	'United Kingdom','***REMOVED***','***REMOVED***','BST', True);
+
+INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
+	[Country], [Switchboard], [Fax], [TimeZone], 
+	[Operating]) VALUES ('Melbourne','***REMOVED*** ***REMOVED***',
+	'Australia','***REMOVED***','','AEST', True);
+
+INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
+	[Country], [Switchboard], [Fax], [TimeZone], 
+	[Operating]) VALUES ('Santa Monica','***REMOVED*** ***REMOVED***',
+	'United States','***REMOVED***','***REMOVED***','PST', True);
+
+INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
+	[Country], [Switchboard], [Fax], [TimeZone], 
+	[Operating]) VALUES ('Singapore','***REMOVED*** ***REMOVED***',
+	'Singapore','***REMOVED***','***REMOVED***','SGT', True);
+
+INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
+	[Country], [Switchboard], [Fax], [TimeZone], 
+	[Operating]) VALUES ('Sydney','***REMOVED*** ***REMOVED***',
+	'Australia','***REMOVED***','***REMOVED***','AEST', True);
+
+INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
+	[Country], [Switchboard], [Fax], [TimeZone], 
+	[Operating]) VALUES ('Tokyo','***REMOVED*** ***REMOVED*** ***REMOVED***',
+	'Japan','***REMOVED***','***REMOVED***','JST', True);
+
+INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
+	[Country], [Switchboard], [Fax], [TimeZone], 
+	[Operating]) VALUES ('Toronto','***REMOVED*** ***REMOVED***',
+	'Canada','***REMOVED***','***REMOVED***','EDT', True);
+
+INSERT INTO [OfficeLocation].[Offices] ([Name], [Address], 
+	[Country], [Switchboard], [Fax], [TimeZone], 
+	[Operating]) VALUES ('Vancouver','***REMOVED*** ***REMOVED******REMOVED***',
+	'Canada','***REMOVED***','***REMOVED***','PDT', True);
