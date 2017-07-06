3f005f75c4441f0c6ee76edb423627974cb978cd
Max Booth <Max.Booth@dimensional.com>
2017-07-06 14:42:33 -0500

    copying deploy files

--- deploy/Web/TeamCity_WebDeploy_Stg.ps1
+++ deploy/Web/TeamCity_WebDeploy_Stg.ps1
@@ -21,9 +21,12 @@ Else {
 Write-Host "copying from $teamCityFileLocation\* to  $remoteServerPath"
 Copy-Item "$teamCityFileLocation\*" $remoteServerPath -recurse
 
+#copies deploy files from team city agent into temporary working directory
+Write-Host "copying from .\deploy\* to $remoteServerPath\deploy\*"
+Copy-Item ".\deploy\*" "$remoteServerPath\deploy\*" -recurse -Force
+
 $sess = New-PSSession -ComputerName $targetServerName 
 write-host "##teamcity[progressStart 'Install of Office Location Microservice to $targetServerName']"
-
 
 Invoke-Command -Session $sess -ArgumentList ($ProjectName)  -Scriptblock { 
     $ProjectName = $($args[0])
