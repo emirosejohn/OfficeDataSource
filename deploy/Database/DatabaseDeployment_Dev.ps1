c2ceeeda60d7132f6b7d28577216a7ea2673719b
Max Booth <Max.Booth@dimensional.com>
2017-07-06 13:41:08 -0500

    changing directory

---
+++ deploy/Database/DatabaseDeployment_Dev.ps1
@@ -0,0 +1,28 @@
+﻿param(
+	[String]$projectName="OfficeLocationMicroservice"
+)
+cls
+
+# '[p]sake' is the same as 'psake' but $Error is not polluted
+remove-module [p]sake
+
+# find psake's path
+
+cd "c:\temp\$ProjectName"
+
+$psakeModule = (Get-ChildItem (".\lib\psake\psake.psm1")).FullName | Sort-Object $_ | select -last 1
+ 
+Import-Module $psakeModule
+
+# you can put arguments to task in multiple lines using `
+Invoke-psake -buildFile .\deploy\Database\default.ps1 `
+			 -parameters @{
+				 "enviornment" = "DEV"
+                 "databaseServer" = "***REMOVED***"
+                 "projectName" = "$projectName"
+				}
+
+Write-Host "Build exit code:" $LastExitCode
+
+# Propagating the exit code so that builds actually fail when there is a problem
+exit $LastExitCode
