263a12fdcdce331a8e926a24a4136fe7f433060b
Max Booth <Max.Booth@dimensional.com>
2017-07-07 13:06:09 -0500

    deleted unneeded file

--- src/WebUI/Models/OfficeModel.cs
+++ src/WebUI/Models/OfficeModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using OfficeLocationMicroservice.Core;
+using OfficeLocationMicroservice.Core.OfficeLocationContext;
 
 namespace OfficeLocationMicroservice.WebUi.Models
 {
