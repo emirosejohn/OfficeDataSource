2f522fa54019e18b6e1d258c644e7e50eb6a56b6
Emily Johnson <Emily.Johnson@dimensional.com>
2017-07-07 14:31:39 -0500

    set up test dependant files. Need to figure out what that sql string does

---
+++ src/IntegrationTests/AllTablesDeleter.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dapper;
+using System.Threading.Tasks;
+using Dimensional.TinyReturns.IntegrationTests;
+
+namespace OfficeLocationMicroservice.IntegrationTests
+{
+    class AllTablesDeleter
+    {
+        public class TableInfoDto
+        {
+
+            public TableInfoDto()
+            {
+
+            }
+            //going to be [OfficeLocation].[Office] unless add more tables
+            public TableInfoDto(string schemaName, string tableName)
+            {
+                SchemaName = schemaName;
+                TableName = tableName;
+            }
+
+            public string TableName { get; set; }
+            public string SchemaName { get; }
+
+            protected bool Equals(TableInfoDto other)
+            {
+                return string.Equals(SchemaName, other.SchemaName) && string.Equals(TableName, other.TableName);
+            }
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((TableInfoDto)obj);
+            }
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return ((SchemaName != null ? SchemaName.GetHashCode() : 0) * 397) ^ (TableName != null ? TableName.GetHashCode() : 0);
+                }
+            }
+        }
+
+        public void DeleteAllDataFromTables(
+            string connectionString,
+            TableInfoDto[] tablesToSkip)
+        {
+            //Idk what the sql string shoudl do here.
+
+            const string sql = @"
+
+
+";
+            IEnumerable<TableInfoDto> tableInfoDtos = null;
+            SqlDatabaseHelper.ConnectionExecuteWithLog(
+                connectionString,
+                connection =>
+                {
+                    tableInfoDtos = connection.Query<TableInfoDto>(sql);
+                },
+                sql);
+
+            var stringBuilder = new StringBuilder();
+
+            foreach (var tableInfoDto in tableInfoDtos)
+            {
+                if (tablesToSkip.Any(skip => skip.Equals(tableInfoDto)))
+                    continue;
+
+                stringBuilder.AppendFormat("DELETE FROM [{0}].[{1}]", tableInfoDto.SchemaName, tableInfoDto.TableName);
+            }
+
+            SqlDatabaseHelper.ConnectionExecuteWithLog(
+                connectionString,
+                connection =>
+                {
+                    connection.Execute(stringBuilder.ToString());
+                },
+                stringBuilder.ToString());
+        }
+    }
+}
---
+++ src/IntegrationTests/Web/Controllers/TestHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeLocationMicroservice.Database;
+using OfficeLocationMicroservice.WebUi;
+
+namespace OfficeLocationMicroservice.IntegrationTests.Web.Controllers
+{
+    public class TestHelper
+    {
+        private readonly OfficeDataTableGateway _officeDataTableGateway;
+
+        public TestHelper()
+        {
+            var databaseSettings = new DatabaseSettings();
+        }
+    }
+}
