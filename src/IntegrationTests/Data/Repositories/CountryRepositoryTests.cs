32064be6a2d85686d29ec41f4c3f25b0e0e4ba97
Max Booth <Max.Booth@dimensional.com>
2017-07-11 08:24:39 -0500

    added uniqueness tests

--- src/IntegrationTests/Data/Repositories/CountryRepositoryTests.cs
+++ src/IntegrationTests/Data/Repositories/CountryRepositoryTests.cs
@@ -29,5 +29,44 @@ namespace OfficeLocationMicroservice.IntegrationTests.Data.Repositories
 
             countries.Length.Should().BeGreaterThan(0);
         }
+
+        [Fact]
+        public void CountriesShouldBeUnique()
+        {
+            var countries = _countryRepository.getAllCountries();
+
+            countries.Should().NotBeNull();
+
+            countries.Length.Should().BeGreaterThan(0);
+
+            var result = UniqueCountriesCheck(countries);
+
+            result.Should().BeTrue();
+        }
+
+        private static bool UniqueCountriesCheck(Country[] countries)
+        {
+            var uniquecountries = new List<Country>();
+
+            foreach (var country in countries)
+            {
+                if (uniquecountries.Contains(country))
+                {
+                    return false;
+                }
+                uniquecountries.Add(country);
+            }
+            return true;
+        }
+
+        [Fact(Skip = "Number of countries may change")]
+        public void CountriesShouldBe137()
+        {
+            var countries = _countryRepository.getAllCountries();
+
+            countries.Should().NotBeNull();
+
+            countries.Length.Should().Be(137);
+        }
     }
 }
