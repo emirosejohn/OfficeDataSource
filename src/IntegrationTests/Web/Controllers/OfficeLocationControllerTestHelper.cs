4e327696ee427f7cee0abd5bfef71675cc0682b2
Max Booth <Max.Booth@dimensional.com>
2017-07-11 09:18:22 -0500

    fixed parallelism in code

--- src/IntegrationTests/Web/Controllers/OfficeLocationControllerTestHelper.cs
+++ src/IntegrationTests/Web/Controllers/OfficeLocationControllerTestHelper.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Web.Mvc;
 using FluentAssertions;
 using OfficeLocationMicroservice.Core.Domain.CountryContext;
 using OfficeLocationMicroservice.Core.Domain.OfficeLocationContext;
-using OfficeLocationMicroservice.Core.Services.CountryFetcher.CountryWebApi;
 using OfficeLocationMicroservice.Core.Services.SharedContext.OfficeLocationDatabase;
 using OfficeLocationMicroservice.Data.CountryWebApi;
 using OfficeLocationMicroservice.Data.OfficeLocationDatabase;
 using OfficeLocationMicroservice.WebUi.Controllers;
 using OfficeLocationMicroservice.WebUi.Models;
-using Country = OfficeLocationMicroservice.Core.Services.CountryFetcher.CountryWebApi.Country;
 
 namespace OfficeLocationMicroservice.IntegrationTests.Web.Controllers
 {
@@ -18,7 +15,7 @@ namespace OfficeLocationMicroservice.IntegrationTests.Web.Controllers
     {
         private readonly AllTablesDeleter _allTablesDeleter;
         private readonly OfficeDataTableGateway _officeDataTableGateway;
-        private readonly CountryWebApiGatewayFake _countryWebApiGateway;
+        private readonly CountryWebApiGatewayStub _countryWebApiGateway;
 
         public TestHelper()
         {
@@ -31,7 +28,7 @@ namespace OfficeLocationMicroservice.IntegrationTests.Web.Controllers
 
             _officeDataTableGateway = new OfficeDataTableGateway(databaseSettings, systemLogForIntegrationTests);
 
-            _countryWebApiGateway = new CountryWebApiGatewayFake();
+            _countryWebApiGateway = new CountryWebApiGatewayStub();
         }
 
         public OfficeLocationRepository GetOfficeLocationRepository()
@@ -81,110 +78,6 @@ namespace OfficeLocationMicroservice.IntegrationTests.Web.Controllers
             var officeModel = (OfficeLocationModel) viewResult.Model;
 
             return officeModel;
-        }
-
-    }
-
-    internal class CountryWebApiGatewayFake : ICountryWebApiGateway
-    {
-        public RegionSchemeResponseJson GetRegionScheme()
-        {
-            var regionScheme = new RegionSchemeResponseJson();
-
-            regionScheme.Regions = new List<Region>();
-
-            var Region1 = new Region()
-            {
-                RegionId = 1,
-                Name = "Region1",
-                Slug = "R1",
-                Countries = new List<Country>()
-            };
-
-            var Region2 = new Region()
-            {
-                RegionId = 1,
-                Name = "Region2",
-                Slug = "R2",
-                Countries = new List<Country>()
-            };
-
-            var Region3 = new Region()
-            {
-                RegionId = 1,
-                Name = "Region3",
-                Slug = "R3",
-                Countries = new List<Country>()
-            };
-
-            var country1 = new Country()
-            {
-                  CountryId = 1,
-                  Name  = "Country 1",
-                  Slug = "1",
-                  ISOCountryCode = "1",
-                  DFACountryCode = "1"
-            };
-
-            var country2 = new Country()
-            {
-                CountryId = 2,
-                Name = "Country 2",
-                Slug = "2",
-                ISOCountryCode = "2",
-                DFACountryCode = "2"
-            };
-
-            var country3 = new Country()
-            {
-                CountryId = 3,
-                Name = "Country 3",
-                Slug = "3",
-                ISOCountryCode = "3",
-                DFACountryCode = "3"
-            };
-
-            var country4 = new Country()
-            {
-                CountryId = 4,
-                Name = "Country 4",
-                Slug = "4",
-                ISOCountryCode = "4",
-                DFACountryCode = "4"
-            };
-
-            var country5 = new Country()
-            {
-                CountryId = 5,
-                Name = "Country 5",
-                Slug = "5",
-                ISOCountryCode = "5",
-                DFACountryCode = "5"
-            };
-
-            var country6 = new Country()
-            {
-                CountryId = 6,
-                Name = "Country 6",
-                Slug = "6",
-                ISOCountryCode = "6",
-                DFACountryCode = "6"
-            };
-
-            Region1.Countries.Add(country1);
-            Region1.Countries.Add(country2);
-
-            Region2.Countries.Add(country3);
-            Region2.Countries.Add(country4);
-
-            Region3.Countries.Add(country5);
-            Region3.Countries.Add(country6);
-
-            regionScheme.Regions.Add(Region1);
-            regionScheme.Regions.Add(Region2);
-            regionScheme.Regions.Add(Region3);
-
-            return regionScheme;
         }
     }
 }
