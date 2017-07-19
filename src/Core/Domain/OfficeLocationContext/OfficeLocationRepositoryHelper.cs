05c7344fed73c4acfa6ca82c30f7586627ed2a3c
Max Booth <Max.Booth@dimensional.com>
2017-07-19 13:08:00 -0500

    changed update email

--- src/Core/Domain/OfficeLocationContext/OfficeLocationRepositoryHelper.cs
+++ src/Core/Domain/OfficeLocationContext/OfficeLocationRepositoryHelper.cs
@@ -29,25 +29,31 @@ namespace OfficeLocationMicroservice.Core.Domain.OfficeLocationContext
             string changedName = newOfficeLocation.Name;
             if (newOfficeLocation.Name != originalOfficeLocation.Name)
             {
-                changedName = "<font color='red'>" + newOfficeLocation.Name + "</font>";
+                changedName = "<span style='color:red;font-weight:bold;'>" + newOfficeLocation.Name + "</span>";
             }
 
             string changedAddress = newOfficeLocation.Address;
             if (newOfficeLocation.Address != originalOfficeLocation.Address)
             {
-                changedAddress = "<font color='red'>" + newOfficeLocation.Address + "</font>";
+                changedAddress = "<span style='color:red;font-weight:bold;'>" + newOfficeLocation.Address + "</span>";
             }
 
             string changedSwitchboard = newOfficeLocation.Switchboard;
             if (newOfficeLocation.Switchboard != originalOfficeLocation.Switchboard)
             {
-                changedSwitchboard = "<font color='red'>" + newOfficeLocation.Switchboard + "</font>";
+                changedSwitchboard = "<span style='color:red;font-weight:bold;'>" + newOfficeLocation.Switchboard + "</span>";
             }
 
             string changedFax = newOfficeLocation.Fax;
             if (newOfficeLocation.Fax != originalOfficeLocation.Fax)
             {
-                changedFax = "<font color='red'>" + newOfficeLocation.Fax + "</font>";
+                changedFax = "<span style='color:red;font-weight:bold;'>" + newOfficeLocation.Fax + "</span>";
+            }
+
+            string changedOperating = newOfficeLocation.Operating;
+            if (newOfficeLocation.Operating != originalOfficeLocation.Operating)
+            {
+                changedOperating = "<span style='color:red;font-weight:bold;'>" + newOfficeLocation.Operating + "</span>";
             }
 
             string body = @"
@@ -56,12 +62,41 @@ namespace OfficeLocationMicroservice.Core.Domain.OfficeLocationContext
                 The following update was just made to the Office Data Source system:  <br />
                        <br />
                 Office: {0} <br />
-                        <br />
-                {1}     <br />
-                {2}     <br />
-                {3}     <br />
-                {4}     <br />
-                        <br />
+
+                    <table>
+                        <tr>
+                            <th></th>
+                            <th><u>old</u></th>
+                            <th><u>updated</u></th>
+                        </tr>
+                        <tr>
+                            <td>Office Name:</td>
+                            <td>{1}</td>
+                            <td>{2}</td>
+                        </tr>
+                        <tr>
+                            <td>Address: </td>
+                            <td>{3}</td>
+                            <td>{4}</td>
+                        </tr>
+                        <tr>
+                            <td>Phone: </td>
+                            <td>{5}</td>
+                            <td>{6}</td>
+                        </tr>
+                        <tr>
+                            <td>Fax: </td>
+                            <td>{7}</td>
+                            <td>{8}</td>
+                        </tr>
+                        <tr>
+                            <td>Operating Status: </td>
+                            <td>{9}</td>
+                            <td>{10}</td>
+                        </tr>
+                    </table>
+
+                        <br /><br>
                 Please make sure that your system reflects this change, and that the proper employees are notified of the change. If you have any questions regarding this change, please reach out to the Corporate Services/Technology team for help.
                         <br /><br />
                 Thanks! <br />
@@ -69,8 +104,13 @@ namespace OfficeLocationMicroservice.Core.Domain.OfficeLocationContext
                 ODS Team
              ";
 
-            body = string.Format(body, changedName, changedName,
-                changedAddress, changedSwitchboard, changedFax);
+            body = string.Format(
+                body, originalOfficeLocation.Name,
+                originalOfficeLocation.Name, changedName,
+                originalOfficeLocation.Address, changedAddress,
+                originalOfficeLocation.Switchboard, changedSwitchboard,
+                originalOfficeLocation.Fax, changedFax,
+                originalOfficeLocation.Operating, changedOperating);
 
             return body;
         }
@@ -84,11 +124,41 @@ namespace OfficeLocationMicroservice.Core.Domain.OfficeLocationContext
                        <br />
                 Office: {0} <br />
                         <br />
-                {1}     <br />
-                {2}     <br />
-                {3}     <br />
-                {4}     <br />
-                        <br />
+
+                    <table>
+                        <tr>
+                            <th></th>
+                            <th><u>old</u></th>
+                            <th><u>updated</u></th>
+                        </tr>
+                        <tr>
+                            <td>Office Name:</td>
+                            <td>{1}</td>
+                            <td>{2}</td>
+                        </tr>
+                        <tr>
+                            <td>Address: </td>
+                            <td>{3}</td>
+                            <td>{4}</td>
+                        </tr>
+                        <tr>
+                            <td>Phone: </td>
+                            <td>{5}</td>
+                            <td>{6}</td>
+                        </tr>
+                        <tr>
+                            <td>Fax: </td>
+                            <td>{7}</td>
+                            <td>{8}</td>
+                        </tr>
+                        <tr>
+                            <td>Operating Status: </td>
+                            <td>{9}</td>
+                            <td>{10}</td>
+                        </tr>
+                    </table>
+
+                        <br /><br />
                 Please make sure that your system reflects this change, and that the proper employees are notified of the change. If you have any questions regarding this change, please reach out to the Corporate Services/Technology team for help.
                         <br /><br />
                 Thanks! <br />
