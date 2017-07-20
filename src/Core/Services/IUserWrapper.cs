c34ff5d94be861fb2f1e3f7780d1065623782c63
Max Booth <Max.Booth@dimensional.com>
2017-07-20 14:30:26 -0500

    got rid of the office with enumerations

---
+++ src/Core/Services/IUserWrapper.cs
@@ -0,0 +1,20 @@
+﻿namespace OfficeLocationMicroservice.Core.Services
+{
+    public interface IUserWrapper
+    {
+        string CurrentUser { get; }
+
+        bool IsInGroup(string groupName);
+
+        IGroupNameConstants GroupNameConstants { get; }
+    }
+
+    public static class UserWrapperExtensions
+    {
+        public static bool IsInAdminGroup(
+            this IUserWrapper user)
+        {
+            return user.IsInGroup(user.GroupNameConstants.AdminGroup);
+        }
+    }
+}
---
+++ src/WebUI/Helpers/UserWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using OfficeLocationMicroservice.Core.Services;
+
+namespace OfficeLocationMicroservice.WebUi.Helpers
+{
+    public class UserWrapper : IUserWrapper
+    {
+        public string CurrentUser
+        {
+            get { return HttpContext.Current.User.Identity.Name; }
+        }
+
+        public bool IsInGroup(string groupName)
+        {
+            return HttpContext.Current.User.IsInRole(groupName);
+        }
+
+        public IGroupNameConstants GroupNameConstants { get; set; }
+    }
+}
