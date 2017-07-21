51d1d557c8a70168031abbb8246367c28ba40818
Max Booth <Max.Booth@dimensional.com>
2017-07-21 17:02:42 -0500

    made == operator

--- src/Core/Domain/OfficeLocationContext/OfficeLocation.cs
+++ src/Core/Domain/OfficeLocationContext/OfficeLocation.cs
@@ -1,4 +1,7 @@
-﻿namespace OfficeLocationMicroservice.Core.Domain.OfficeLocationContext
+﻿using System;
+using System.ComponentModel;
+
+namespace OfficeLocationMicroservice.Core.Domain.OfficeLocationContext
 {
     public class OfficeLocation
     {
@@ -17,5 +20,33 @@
         public string Operating { get; set; }
 
         public string HasChanged { get; set; }
+
+
+        public static bool operator ==(OfficeLocation x, OfficeLocation y)
+        {
+            if ((object)x==null && (object)y == null) //checks for nulls
+            {
+                return true
+            }
+            else if ((object) x == null || (object) y == null)
+            {
+                return false;
+            }
+
+
+                return (
+                x.OfficeId == y.OfficeId &&
+                 x.Operating == y.Operating &&
+                 x.Address == y.Address &&
+                 x.Country == y.Country &&
+                 x.Fax == y.Fax &&
+                 x.Name == y.Name &&
+                 x.Switchboard == y.Switchboard);
+        }
+
+        public static bool operator !=(OfficeLocation x, OfficeLocation y)
+        {
+            return !(x == y);
+        }
     }
 }
